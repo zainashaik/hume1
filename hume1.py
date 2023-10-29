b7277d7983c1226d38a7a19f468ed72ff34c9caa
Zaina Shaik <zainashaik02@gmail.com>
2023-10-29 10:32:14 -0700

    added first hume file

---
+++ hume1.py
@@ -0,0 +1,14 @@
+from hume import HumeBatchClient
+from hume.models.config import FaceConfig
+
+client = HumeBatchClient("p5hIRUVXHGy3QbOJYBDqh153XCPIFlT7ScbmZ97auTRD36yy")
+urls = ["https://hume-tutorials.s3.amazonaws.com/faces.zip"]
+config = FaceConfig()
+job = client.submit_job(urls, [config])
+
+print(job)
+print("Running...")
+
+details = job.await_complete()
+job.download_predictions("predictions.json")
+print("Predictions downloaded to predictions.json")
